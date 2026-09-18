076893b2e69ccaaabefdb3da563ef9a01c8df0df
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 14:03:40 +0600

    constant file

---
+++ DATATYPE.C/number.c/CTYPECONVERSITION.C/cconstant.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    const int minutesPerHour = 60;
+    const int monthsInYear = 12;
+
+    printf("Minutes in an hour: %d\n", minutesPerHour);
+    printf("Months in a year: %d\n", monthsInYear);
+
+    return 0;
+}
