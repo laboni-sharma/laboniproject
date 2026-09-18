ef1daf70a8b5d2c6aa0c6aa390f6e303ad9e4c3f
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 14:42:10 +0600

    nested if file

---
+++ DATATYPE.C/number.c/CTYPECONVERSITION.C/COPERATORS.C/operators.c/operators.c/logical.c/CBOOLEANS.C/booleans.c/IF..ELSE.C/ELSE.C/ELSE..IF.C/SHORTHANDELSEIS.C/NESTEDIF.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x = 15;
+    int y = 25;
+
+    if (x > 10) {
+        printf("x is greater than 10\n");
+
+        // Nested if
+        if (y > 20) {
+            printf("y is also greater than 20\n");
+        }
+    }
+
+    return 0;
+}
