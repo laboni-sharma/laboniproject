2f25c0ba46171d34eb4c9938acec17bd883a8a68
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 14:12:28 +0600

    exp one file

---
+++ DATATYPE.C/number.c/CTYPECONVERSITION.C/COPERATORS.C/operators.c/operators.c/assignment.c/exmpleone.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int savings = 100; 
+    savings += 50; // savings = savings + 50 (100 + 50 = 150)
+    
+    printf("Total savings: %d\n", savings);
+
+    return 0;
+}
