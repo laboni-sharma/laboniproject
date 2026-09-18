be975f9f30ff1702cfb268184be0edbe587320f3
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 13:35:48 +0600

    variables one file

---
+++ variabiles.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // Declare a variable
+    int myNum;
+
+    // Assign a value later
+    myNum = 15;
+
+    // Output dekhanor jonno
+    printf("%d", myNum);
+
+    return 0;
+}
