3bb1f3a539c8ffd52952b269cff521218d4b286b
laboni-sharma <inobalsharma2006@gmail.com>
2026-09-18 16:05:25 +0600

    while loop one file

---
+++ DATATYPE.C/number.c/CTYPECONVERSITION.C/COPERATORS.C/operators.c/operators.c/logical.c/CBOOLEANS.C/booleans.c/IF..ELSE.C/ELSE.C/ELSE..IF.C/SHORTHANDELSEIS.C/LOGICALOPERATIN.C/OR.C/CSWITCH.C/LOOPS.C/WHILELOOPS.C/whileloopone.c
@@ -0,0 +1,13 @@
+#include <stdio.stdio.h> // Header file for input/output functions
+
+int main() {
+    int i = 0; // Initialize loop counter
+
+    // While loop execution
+    while (i < 5) {
+        printf("%d\n", i);
+        i++; // Increment counter
+    }
+
+    return 0; // Standard exit code for success
+}
